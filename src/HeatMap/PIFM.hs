a3b0ec3a7bba9770758a6e281d4e95233c6eef08
Chris Denniston <octopuscabbage@gmail.com>
2015-08-18 15:43:41 -0500

    went to crows

--- src/HeatMap/PIFM.hs
+++ src/HeatMap/PIFM.hs
@@ -6,14 +6,19 @@ import Util.Utils
 type HeatMap = [Int]
 
 emptyHeatMap:: Reader State HeatMap
-emptyHeatMap = getBoardSize >>= \size -> map (map (const 0) [0..size]) [0..size]
+emptyHeatMap = getBoardSize >>= \size -> map (map (const 0) [0..size]) [0..size] --should be list comp probably
 
 createHeatMap:: MapValues -> Reader State HeatMap
 createHeatMap mapValues = do tiles <- getTiles
-                          heatableTiles <- filterTiles (\t -> t /= FreeTile && t /= )
-                          compute 
+                             heatableTiles <- filterTiles (\t -> t /= FreeTile && t /= )
+                             return emptyHeatMap
 
 -- | Heats all the tiles in a circle around using falloff, so a tile 1 distance away will heat heatValue - 1
 heatPoint:: Int -> Point -> HeatMap -> Reader State HeatMap
-heatPoint heatValue (Point x y) heatMap = allPoints >>= \points -> foldl1' (cur) heatmap
+heatPoint heatValue po heatMap = do
+  points <- allPoints
+  board <- getBoard
+  map (mapF) $ zip heatMap board
+   where mapF (curval,pb) = let distance = diagonalDistance po pb in if distance <= heatValue then curVal + (heatVal - distance) else curVal
+    
                        
